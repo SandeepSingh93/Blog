a81ce244b6d0c7ea38fa1196217fde7303f35b3d
sandeepsingh93 <sandeepsingh.jh@gmail.com>
2015-10-23 02:02:38 +0530

    update according to heroku

--- launchyard/settings.py
+++ launchyard/settings.py
@@ -79,7 +79,7 @@ TEMPLATES = [
 ]
 
 #WSGI_APPLICATION = 'launchyard.wsgi.application'
-
+from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 application = Cling(get_wsgi_application())
 
